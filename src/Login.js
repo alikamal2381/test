537230b9f1f3a7eb7db78aa1896080a6bd006e26
ALI KAMAL <alikamal2381@gmail.com>
2020-08-21 20:41:29 +0500

    final

---
+++ src/Login.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import './Login.css';
+
+function Login() {
+  return (
+    <div className="Login">
+      <header className="Login-header">
+            LOGIN
+
+
+
+      </header>
+    </div>
+  );
+}
+
+export default Login;
